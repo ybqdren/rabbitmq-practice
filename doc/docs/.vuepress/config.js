134bd0523f7723bbc4ba066465406888aecc3c22
ybqdren <withzhaowen@126.com>
2022-10-01 16:50:21 +0800

    chore docs

---
+++ doc/docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'RabbitMQ Practice',
+    description: 'RabbitMQ 实践',
+    base: "/rabbitmq-practice/",
+    themeConfig: {
+        logo: '/logo.png',
+        lastUpdated: '最后更新时间',
+        sidebar: 'auto',
+        nav: [
+            {
+                text: '首页',
+                link: '/',
+            }
+        ],
+    },
+}
